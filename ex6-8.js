960b5ca97f78c58e2c0767df48b2342cd37b09e6
taka0621nnb <zm20359@josai.ac.jp>
2021-11-16 01:45:19 +0000

    11/16

---
+++ ex6-8.js
@@ -0,0 +1,10 @@
+function sayhello68() {
+    alert('heiio, ' + document.getElementById('who68').innerHTML+ 
+    '!');
+}
+    function taro68() {
+    document.getElementById('who68').innerHTML= '太郎';
+}
+function hanako68() {
+        document.getElementById('who68').innerHTML= '花子'
+    }
