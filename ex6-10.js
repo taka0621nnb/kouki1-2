03b5b549478b2f9923dcfd89542ceca583e75f48
taka0621nnb <zm20359@josai.ac.jp>
2021-12-07 01:58:52 +0000

    1207

--- ex6-10.js
+++ ex6-10.js
@@ -2,3 +2,9 @@ function imgchange1() {
     document.getElementById('logo').src= 
     './image/城西.bmp';
 }
+
+function imgchange2() {
+    document.getElementById('logo').src= 
+    './image/keiei_small.jpg';
+}
+
